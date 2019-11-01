59cbfd2d1fe55ac9136123c2ff4207d1dc7b9f2a
Mark Handley <mark@handley.org.uk>
2019-11-01 01:19:41 +0000

    Add colour to blocks.  Not really essential, but makes it look prettier, and useful for manual play to distingish similar pieces quickly.
    Fix bug in cmdline.py, where the game will crash if the piece lands
    due to a timed drop.

--- cmdline.py
+++ cmdline.py
@@ -13,7 +13,20 @@ COLOR_WALL = 1
 COLOR_BLOCK = 2
 COLOR_CELL = 3
 COLOR_NOTHING = 4
-
+COLOR_RED = 5
+COLOR_ORANGE = 6
+COLOR_YELLOW = 7
+COLOR_GREEN = 8
+COLOR_CYAN = 9
+COLOR_BLUE = 10
+COLOR_MAGENTA = 11
+COLOR_NAMES = {"red": COLOR_RED,
+               "orange": COLOR_ORANGE,
+               "yellow": COLOR_YELLOW,
+               "green": COLOR_GREEN,
+               "cyan": COLOR_CYAN,
+               "blue": COLOR_BLUE,
+               "magenta": COLOR_MAGENTA}
 
 def paint(window, x, y, color, count=1):
     window.addstr(y, x*2, '  ' * count, curses.color_pair(color))
@@ -25,34 +38,25 @@ def render(window, board):
     """
 
     for y in range(board.height):
-        # Draw part of the left wall.
-        paint(window, 0, y, COLOR_WALL)
-
         # Draw each individual row
         for x in range(board.width):
             if board.falling is not None and (x, y) in board.falling:
                 # Location is occupied by falling block
-                color = COLOR_BLOCK
+                color = COLOR_NAMES[board.falling.color]
             elif (x, y) in board:
                 # Location is occupied by fallen block
-                color = COLOR_CELL
+                color = COLOR_NAMES[board.cellcolor[(x,y)]]
             else:
                 # There is nothing here.
                 color = COLOR_NOTHING
             paint(window, x+1, y, color)
-
-        # Draw part of right wall.
-        paint(window, x+2, y, COLOR_WALL)
-
-    # Draw the bottom wall
-    paint(window, 0, y+1, COLOR_WALL, count=board.width+2)
 
     # Draw the next piece
 
     for y in range(6):
         for x in range(4):
             if (x, y) in board.next:
-                color = COLOR_BLOCK
+                color = COLOR_NAMES[board.next.color]
             else:
                 color = COLOR_NOTHING
 
@@ -60,11 +64,34 @@ def render(window, board):
 
     # Draw the score line below the window.
     window.addstr(
-        board.height+1,
+        board.height+2,
         0,
         f'Score: {board.score} ',
         curses.color_pair(COLOR_NOTHING)
     )
+
+    # Draw the board frame
+    window.move(0,0)
+    window.vline(curses.ACS_VLINE, board.height+2)
+    window.move(0,1)
+    window.vline(curses.ACS_VLINE, board.height+1)
+    window.addch(0, 0, curses.ACS_ULCORNER)
+    window.addch(0, 1, curses.ACS_URCORNER)
+    window.move(0,board.width*2+2)
+    window.vline(curses.ACS_VLINE, board.height+1)
+    window.move(0,board.width*2+3)
+    window.vline(curses.ACS_VLINE, board.height+2)
+    window.addch(0, board.width*2+2, curses.ACS_ULCORNER)
+    window.addch(0, board.width*2+3, curses.ACS_URCORNER)
+    window.move(board.height+1, 0)
+    window.hline(curses.ACS_HLINE, board.width*2+3)
+    window.move(board.height, 1)
+    window.hline(curses.ACS_HLINE, board.width*2+1)
+    window.addch(board.height+1, 0, curses.ACS_LLCORNER)
+    window.addch(board.height, 1, curses.ACS_LLCORNER)
+    window.addch(board.height+1, board.width*2+3, curses.ACS_LRCORNER)
+    window.addch(board.height, board.width*2+2, curses.ACS_LRCORNER)
+    window.move(board.height+2, 0)
 
     window.refresh()
 
@@ -77,9 +104,15 @@ class UserPlayer(Player):
     screen = None
 
     def __init__(self, window):
+        global force_down
         self.window = window
+        force_down = False
 
     def move(self, board):
+        global force_down
+        if force_down:
+            force_down = False
+            return Direction.Down
         key = self.window.getch()
 
         if key == curses.KEY_RIGHT:
@@ -105,9 +138,9 @@ def run(window):
     adversary = RandomAdversary(DEFAULT_SEED)
 
     def force_drop(signum, frame):
+        global force_down
         signal.alarm(1)
-        board.move(Direction.Down)
-        render(window, board)
+        force_down = True
 
     signal.signal(signal.SIGALRM, force_drop)
     signal.alarm(1)
@@ -140,7 +173,7 @@ if __name__ == '__main__':
         curses.cbreak()
 
         window = curses.newwin(
-            BOARD_HEIGHT + 2,
+            BOARD_HEIGHT + 3,
             (BOARD_WIDTH + 2 + 6)*2 + 1
         )
         window.keypad(True)
@@ -150,6 +183,13 @@ if __name__ == '__main__':
         curses.init_pair(COLOR_BLOCK, curses.COLOR_WHITE, curses.COLOR_RED)
         curses.init_pair(COLOR_CELL, curses.COLOR_WHITE, curses.COLOR_WHITE)
         curses.init_pair(COLOR_NOTHING, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(COLOR_RED, curses.COLOR_WHITE, curses.COLOR_RED)
+        curses.init_pair(COLOR_ORANGE, curses.COLOR_WHITE, curses.COLOR_WHITE) #orange not supported
+        curses.init_pair(COLOR_YELLOW, curses.COLOR_WHITE, curses.COLOR_YELLOW)
+        curses.init_pair(COLOR_GREEN, curses.COLOR_WHITE, curses.COLOR_GREEN)
+        curses.init_pair(COLOR_CYAN, curses.COLOR_WHITE, curses.COLOR_CYAN)
+        curses.init_pair(COLOR_BLUE, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(COLOR_MAGENTA, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
 
         run(window)
     finally:
