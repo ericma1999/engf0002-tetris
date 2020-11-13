cd98716a9e1d4dd5af1c13b96de70c902e87cbff
Passawis <official.passawis@gmail.com>
2020-11-14 02:29:05 +0700

    changed heuristic

--- player.py
+++ player.py
@@ -10,8 +10,8 @@ class Player:
         raise NotImplementedError
 class MyPlayer(Player):
     # heuristic constants
-    heightConstant = -0.710066
-    linesConstant = 0.860666
+    heightConstant = -0.510066
+    linesConstant = 1.260666
     holesConstant = -0.35663
     bumpinessConstant = -0.184483
 
