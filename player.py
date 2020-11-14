810bffba43dd50badbb2057b9f3885cbc36e3684
Passawis <official.passawis@gmail.com>
2020-11-14 23:20:54 +0700

    only height run

--- player.py
+++ player.py
@@ -11,14 +11,10 @@ class Player:
 class MyPlayer(Player):
     # heuristic constants
     heightConstant = -0.510066
-    linesConstant = 0.960666
-    holesConstant = -0.75663
+    linesConstant = 1.260666
+    holesConstant = -0.35663
     bumpinessConstant = -0.184483
-    colWithHoles = -0.29445
-    block = 0
-    min_range = 0
-    upper_range = 10
-    comboing = False
+    moves = 0
 
     best_horizontal_position = None
     best_rotation_position = None
@@ -47,33 +43,19 @@ class MyPlayer(Player):
             total += abs(columns[i] - columns[i+1])
         return total * self.bumpinessConstant
 
-    def check_col_with_holes(self, board):
-        total_cols = []
-        for x in range(board.width):
-            for y in range(board.height):
-                if (x, y) not in board.cells:
-                    if (x + 1,y) in board.cells and (x - 1,y) in board.cells and (x, y+1) in board.cells and (x, y-1) in board.cells:
-                        total_cols.append(x)
-        unique_cols = set(total_cols)
-        return len(unique_cols) * self.colWithHoles
-
     def check_lines(self, originalBoard, board):
         score = board.score - originalBoard.score
         complete_line = 0
         # points given
         if score >= 1600:
             complete_line += 4
-            return complete_line * self.linesConstant * 2
         elif score >= 800:
             complete_line += 3
-            return complete_line * self.linesConstant / 2
         elif score >= 400:
             complete_line += 2
-            return complete_line * self.linesConstant / 3
         elif score >= 100:
             complete_line += 1
-            return complete_line * self.linesConstant / 4
-        return complete_line * self.linesConstant
+        return complete_line * self.linesConstant * self.moves
 
     
     def check_holes(self, board):
@@ -86,8 +68,8 @@ class MyPlayer(Player):
         return self.holesConstant * holes
 
     def calc_score(self, originalBoard, board):
-        total = self.check_height(board) + self.check_holes(board) + self.check_lines(originalBoard, board) + self.check_bumpiness(board) 
-        # + self.check_col_with_holes(board)
+        total = self.check_height(board) 
+        # + self.check_holes(board) + self.check_lines(originalBoard, board) + self.check_bumpiness(board)
         #  + self.check_mean_height(board)
         return total
 
@@ -120,31 +102,10 @@ class MyPlayer(Player):
 
     def simulate_best_position(self, board):
         score = None
-        self.block +=1
-        print("block", self.block)
+        self.moves += 1
         for rotation in range(4):
-            for horizontal_moves in range(self.min_range,self.upper_range):
+            for horizontal_moves in range(board.width):
                 cloned_board = board.clone()
-
-                columns = self.generate_column_height(cloned_board)
-                print(columns)
-                if ((sum(columns[0:10]) / 10) >= 10):
-                    # self.min_range = 0
-                    # self.upper_range = 10
-                    self.linesConstant = 1.266006
-                else:
-                    # self.min_range = 3
-                    # self.upper_range = 10
-                    self.linesConstant = 0.960666
-                #     self.min_range = 8
-                #     self.upper_range = 10
-                #     self.linesConstant = 1.5660066
-                # else:
-                #     self.min_range = 0
-                #     self.upper_range = 8
-                #     self.linesConstant = 0.760666
-
-
                 self.try_rotation(rotation, cloned_board)
                 self.try_moves(horizontal_moves, cloned_board)
 
