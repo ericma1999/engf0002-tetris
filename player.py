de8197a9eec24d4beea06a5815a9e11e514e03d3
Passawis <official.passawis@gmail.com>
2020-11-16 18:08:26 +0700

    safer conditions

--- player.py
+++ player.py
@@ -113,21 +113,16 @@ class MyPlayer(Player):
         upper_bound = 10
         lower_bound = 2
         columns = self.generate_column_height(board)
-        more_than_four = [column for column in columns[0:7] if column >= 5]
-        moure_than_five = [column for column in columns[0:7] if column >= 6]
-        moure_than_six = [column for column in columns[0:7] if column >= 7]
+        more_than_four = [column for column in columns if column >= 4]
+        moure_than_eight = [column for column in columns if column >= 5]
         print(sum(columns) / len(columns))
         avg = sum(columns) / len(columns)
-        if(len(more_than_four) >= 8 or len(moure_than_five) >= 4 or avg > 6):
+        if(len(more_than_four) >= 6 or avg > 4.4 or len(moure_than_eight) >= 2):
             upper_bound = 10
             lower_bound = 0
-            self.holesConstant = -0.410066
-            # self.heightConstant = -0.7
-            self.bumpinessConstant = -0.384483
-            
+            self.holesConstant = -1.5666
         else:
-            self.holesConstant = -0.856632
-            self.bumpinessConstant = -0.184483
+            self.holesConstant = -2.0
             upper_bound = 10
             lower_bound = 2
 
